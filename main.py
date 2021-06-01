97a1118801f7c911dca78a6e0a699d6d177e3a69
dizzysufi <>
2021-06-01 19:56:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+#''''
+#for in loop
+#'''
+
+#students = ['bob','adriana','felix']
+
+#for x in students:
+#  print("hi",x)
+#
+
+#print((range)(10))
+#print(list(range(10)))
+#print(list(range(3, 6)))
+#print(list(range(3, 12, 3)))
+###
+
+
+#for x in range(1, 6, 2):
+#  print(x * 2)
+
+#for i in range(1, 6):
+#  for j in range(i):
+#    print("*", end=" ")
+#  print()
+
+
+
+#WHILE LOOPS
+
+#i = 1
+#while i < 10:
+#  print(i)
+#  i += 1 
+
+#break statement
+
+#i = 1
+#while i < 10:
+#  if i == 5:
+#  print(i)
+#    break
+#  i += 1
+
+  #continue statements
+
+  i = 1
+  while i < 5:
+    if i += 1
+    if i == 4:
+      continue
+    print 
+#######
+
+
+
+for i in range (1, 10):
+  if i == 3:
+    break
+  print(i)
